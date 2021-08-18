55a463650f37b0fc02d8f1ce1637a1b3d191a86e
jaymzee <jaymzee@users.noreply.github.com>
2021-08-18 11:59:48 -0400

    added usage (man page)

--- src/index.js
+++ src/index.js
@@ -7,6 +7,31 @@ app.use(express.urlencoded({extended: true}));
 app.use('/static', express.static('public'));
 app.use(express.static('public/uploads'));
 
+man_page = `
+jclbin(1)                        JCLBIN                        jclbin(1)
+
+NAME
+    jclbin: command line pastebin:
+
+SYNOPSIS
+    curl -F 'jclbin=@filename' http://example.com
+
+DESCRIPTION
+    A simple http pastebin server like ix.io, clbin.com, or sprunge.us
+    It currently does not create nice short urls or support curl's
+    redirect from stdin "f=<-" but i'll get around to it.
+
+EXAMPLE
+    $ curl -F 'f=@filename' https://example.com
+    http://example.com/2c42f4fcbd451a05bc904b06eefcc2ee
+
+
+SEE ALSO
+    http://ix.io
+    http://clbin.com
+    http://sprunge.us
+`;
+
 const serverUrl = server => {
   let { address, family, port } = server.address()
   if (family == 'IPv6' && address.includes(':')) {
@@ -16,7 +41,7 @@ const serverUrl = server => {
 };
 
 app.get('/', (req, res) => {
-  res.send('Usage:\n');
+  res.send(man_page);
 });
 
 app.post('/', upload.single('f'), (req, res) => {
